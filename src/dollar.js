51a787f52c60a2bd2e5b9642bbdecf4fe452f5c1
Casaubon <netzach.yoshida@gmail.com>
2018-01-13 17:11:38 +0900

    remove dollar class

--- src/dollar.js
+++ src/dollar.js
@@ -10,11 +10,6 @@ class Dollar extends Money {
   times(multiplier: number) {
     return new Dollar(this.amount * multiplier)
   }
-
-  equals(object: any) {
-    const dollar: Money = (object: Dollar)
-    return this.amount === dollar.amount
-  }
 }
 
 export default Dollar
