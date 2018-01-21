60691a525b63de169cdf18eccab93c39e9551800
Casaubon <netzach.yoshida@gmail.com>
2018-01-21 18:03:13 +0900

    add equal method

--- src/dollar.js
+++ src/dollar.js
@@ -10,6 +10,10 @@ class Dollar extends Money {
   times(multiplier: number) {
     return new Dollar(this.amount * multiplier)
   }
+
+  equals(object: any) {
+    return this.amount === object.amount
+  }
 }
 
 export default Dollar
