40b32795914bef2e583cddab78dc91bc4732f865
Casaubon <netzach.yoshida@gmail.com>
2018-01-21 18:16:22 +0900

    remove equals method

--- src/dollar.js
+++ src/dollar.js
@@ -10,10 +10,6 @@ class Dollar extends Money {
   times(multiplier: number) {
     return new Dollar(this.amount * multiplier)
   }
-
-  equals(object: any) {
-    return this.amount === object.amount
-  }
 }
 
 export default Dollar
